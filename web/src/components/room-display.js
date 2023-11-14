44830a8ce7193f12c4db29cb53eafac791347154
Vitalii Furmanov <vitaliy.furmanov@gmail.com>
2023-11-14 03:11:35 +0100

    rtc module refactoring

--- web/src/components/room-display.js
+++ web/src/components/room-display.js
@@ -1,83 +1,89 @@
-import constants from '../common/constants';
-import WebRtc  from '../common/network/webrtc';
-import event from '../common/event';
+import constants from "../common/constants";
+import WebRtc from "../common/network/webrtc";
+import notification from "../common/notification";
 import log from "../common/log";
 
-
 class RoomDisplay extends HTMLElement {
-  constructor(){
+  constructor() {
     super();
-    const shadow = this.attachShadow({mode: 'open'});
+    const shadow = this.attachShadow({ mode: "open" });
 
-    const display = document.createElement('div');
-    display.setAttribute('class', 'main-display');
-    const stream = document.createElement('div');
-    stream.setAttribute('id', 'remote-video');
+    const display = document.createElement("div");
+    display.setAttribute("class", "main-display");
+    this.webrtc = new WebRtc(display);
+    this.socket = null;
 
-
-
-    const style = document.createElement('style');
+    const style = document.createElement("style");
     style.textContent = `
     `;
 
     shadow.appendChild(style);
     shadow.appendChild(display);
-    shadow.appendChild(stream);
   }
 
-  connectedCallback(){
+  connectedCallback() {
     this.upgardeConnection();
-
-  }
-  async upgardeConnection(){
-   
-    const shadow = this.shadowRoot;
-    const url = window.location.pathname;
-    const uuid = url.split('/')[2];
-
-    const remoteVideo = shadow.querySelector('#remote-video');
-
-    const webrtc = new WebRtc(remoteVideo);
-    const socket = new WebSocket(`ws://localhost:8000/room/join/${uuid}?username=lock`);
-
-    window.addEventListener('beforeunload', () => {webrtc.stop(), socket.close()});
-    window.addEventListener('unload', () => {webrtc.stop(), socket.close()});
-
-    socket.onopen = () => {
-      log.info ('[ws] Socket is open');
-        
-      event.execute(constants.RTC_SDP_ANSWER_CREATED, (answer => {
-      socket.send(JSON.stringify(answer))
-      log.info('[ws] -> Answer was send');
-      }))
-
-      event.execute(constants.RTC_ICE_CANDIDATE_FOUND, (candidate=> {
-      socket.send(JSON.stringify(candidate));
-      log.info('[ws] -> Candidate was send');
-      }));
-    }
+    notification.execute(constants.event.RTC_INPUT_READY,()=>{
+      console.log(this.webrtc.dataChannel.send("test"))
+    },true);
     
-    socket.onmessage = (e) => {
+  }
+
+
+  upgardeConnection() {
+    
+    const url = window.location.pathname;
+    const uuid = url.split("/")[2];
+    
+    this.socket = new WebSocket(
+      `ws://localhost:8000/room/join/${uuid}?username=lock`
+      );
+
+      this.socket.onopen = () => {
+        log.info("[ws] Socket is open");
+        
+      };
+
+      notification.execute(constants.event.RTC_SDP_ANSWER_CREATED,(answer) => {
+         this.socket.send(JSON.stringify(answer));
+         log.info(`[ws]->Answer was send`);
+      },true);
+      
+    notification.execute(constants.event.RTC_ICE_CANDIDATE_FOUND,(candidate) => {
+      this.socket.send(JSON.stringify(candidate));
+      log.info(`[ws]->Candidate was send`);
+    });
+
+    this.socket.onmessage = (e) => {
       const data = JSON.parse(e.data);
       switch (true) {
-        case data.hasOwnProperty('sdp'):
-          log.info('[ws] <- Offer received');
-           webrtc.start(data);
-           break;
-        
-        case data.hasOwnProperty('candidate'):
-          log.info('[ws] <- Candidate received');
-          webrtc.setCandidate(data);
+        case data.hasOwnProperty("sdp"):
+          log.info("[ws] <- Offer received");
+          this.webrtc.init(data)
           break;
-      
+        case data.hasOwnProperty("candidate"):
+          log.debug(`[ws] <- Candidate received : ${JSON.stringify(data)}`);
+         
+          this.webrtc.remoteCandidates.push(data);
+          break;
+        case data.hasOwnProperty("signal"):
+          if (data.signal === "Server ICE gathering is complete") {
+            log.info("[ws] <- Ready signal received");
+            this.webrtc.addice();
+          }
+          break;
         default:
-          log.warn('[ws] <- Unknown message received');
-          //log.warn(JSON.stringify(e.data))
+          log.warn("[ws] <- Unknown message received");
+          log.warn(JSON.stringify(e.data));
           break;
       }
-    }
-    socket.onclose = () => {log.info ('[ws] Socket is closed');      }
-    socket.onerror = (error) => {log.error (`[ws] Socket error: ${error}`);}
+    };
+    this.socket.onclose = () => {
+      log.info("[ws] Socket is closed");
+    };
+    this.socket.onerror = (error) => {
+      log.error(`[ws] Socket error: ${error}`);
+    };
   }
 }
-customElements.define('room-display', RoomDisplay);
+customElements.define("room-display", RoomDisplay);
